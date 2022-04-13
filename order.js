6cb825d8a4600ea814fe854cdec107c1e3a5e008
DavidB26 <luisdavidneslanga@gmail.com>
2022-04-13 14:05:10 -0500

    Cambios Abril

---
+++ order.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender',
+        productNumber: '36333',
+        paymentStatus: 'Refunde',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko Pro Drone',
+        productNumber: '45678',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96754',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'LG monitor',
+        productNumber: '12553',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+]
